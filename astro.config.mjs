2b31049de4fac6ec497500574c5c2e051f589fe1
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-03-04 14:08:47 -0300

    Refactor configuration and remove unused Tailwind CSS dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
-
-import netlify from '@astrojs/netlify';
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-
-  adapter: netlify()
+  adapter: netlify(),
 });
